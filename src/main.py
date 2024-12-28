1dc8b5b0c3b33b461832f833dda62d6c1669a39a
Rollsocke <r.kubitschek93@gmail.com>
2024-12-28 12:32:58 +0100

    main programm is working

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("hello world")
+from textnode import *
+
+text = "this is text"
+text_type = TextType.BOLD
+url = "www.google.com"
+def main():
+    page = TextNode(text, text_type, url)
+    print(page)
+
+        
+if __name__ == "__main__":
+    main()
