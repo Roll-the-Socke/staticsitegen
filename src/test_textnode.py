9eaf8f1b342b02acea4e44593318f1c7ca7c105c
Rollsocke <r.kubitschek93@gmail.com>
2024-12-28 12:56:12 +0100

    tests finished

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -12,17 +12,17 @@ class TestTextNode(unittest.TestCase):
     def test_text(self):
         node = TextNode("Ich gehe zur Schule", TextType.ITALIC)
         node2 = TextNode("Ich gehe zur Uni", TextType.ITALIC)
-        self.assertEqual(node, node2)
+        self.assertNotEqual(node, node2)
     
     def test_type(self):
         node = TextNode("Ich gehe arbeiten", TextType.BOLD)
         node2 = TextNode("Ich gehe arbeiten", TextType.ITALIC)
-        self.assertEqual(node, node2)
+        self.assertNotEqual(node, node2)
 
     def test_url(self):
         node = TextNode("Ich gehe bowlen", TextType.BOLD, "google.com")
         node2 = TextNode("Ich gehe bowlen", TextType.BOLD)
-        self.assertEqual(node, node2)
+        self.assertNotEqual(node, node2)
 
 
 if __name__ == "__main__":
