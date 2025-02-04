36add84cbb28c2b07c9ae3e6267ad2360e05b5c5
Rollsocke <r.kubitschek93@gmail.com>
2025-02-04 16:37:12 +0100

    added statics and copy static to public function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,39 @@
 from textnode import *
+from htmlnode import *
+from markdowntransformation import *
+import os
+import shutil
 
 text = "this is text"
 text_type = TextType.BOLD
 url = "www.google.com"
+
+def copy_static_to_public(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.makedirs(destination)
+    _recursive_copy(source, destination)
+
+
+def _recursive_copy(source, destination):
+    for item in os.listdir(source):
+        full_path = os.path.join(source, item)
+        dest_path = os.path.join(destination, item)
+        print(f"Found item: {full_path}")
+        if os.path.isfile(full_path):
+            shutil.copy(full_path, dest_path)
+            print(f"Copying file: {full_path} to {dest_path}")
+        elif os.path.isdir(full_path):
+            os.makedirs(dest_path)
+            print(f"Created directory: {dest_path}")
+            _recursive_copy(full_path, dest_path)
+
+
 def main():
-    page = TextNode(text, text_type, url)
-    print(page)
+    source = os.path.expanduser("~/workspace/github.com/Roll-the-Socke/staticsitegen/static")
+    destination = os.path.expanduser("~/workspace/github.com/Roll-the-Socke/staticsitegen/public")
+
+    copy_static_to_public(source, destination)
 
         
 if __name__ == "__main__":
