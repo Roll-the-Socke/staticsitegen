c37912a893579c2e414bd0f362a50609f0476de8
Rollsocke <r.kubitschek93@gmail.com>
2025-02-04 19:26:34 +0100

    enabled handling of multiple pages aka recursively creating pages

--- src/generator.py
+++ src/generator.py
@@ -22,3 +22,17 @@ def generate_page(from_path, template_path, dest_path):
     
     with open(dest_path, "w") as file:
         file.write(final_html)
+
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+    files = os.listdir(dir_path_content)
+    for file in files:
+        full_path = os.path.join(dir_path_content, file)
+        rel_path = os.path.relpath(full_path, dir_path_content)
+        dest_path = os.path.join(dest_dir_path, rel_path)
+        if os.path.isfile(full_path):
+            if full_path.endswith(".md"):
+                os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+                generate_page(full_path, template_path, dest_path)
+        else:
+            os.makedirs(dest_path, exist_ok=True)
+            generate_page_recursive(full_path, template_path, dest_dir_path)
